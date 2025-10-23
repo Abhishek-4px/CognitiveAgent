118d15003dd6bb68fd1a2a618735ee8ebbb172c6
Abhishek Kumar Shukla <abhishekkumarshukla.official@gmail.com>
2025-10-24 03:09:50 +0530

    added redis in config

--- app/config.py
+++ app/config.py
@@ -3,7 +3,8 @@ from pydantic_settings import BaseSettings, SettingsConfigDict
 class Settings(BaseSettings):
     GOOGLE_API_KEY: str
     PINECONE_API_KEY: str
-    DATABASE_URL:str                   #AnyUrl: strictly for https,mysql,postgres links
+    DATABASE_URL:str
+    REDIS_URL: str                   #AnyUrl: strictly for https,mysql,postgres links
     ENVIRONMENT: str = "development"
 
     model_config = SettingsConfigDict(env_file=".env")
